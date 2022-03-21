e9137c22bade9a586eda652f6204c83a9288dc1b
HANGANG <1309991265@qq.com>
2022-03-21 09:27:32 +0800

    表达式求值

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,55 +1,36 @@
 /*
- * @沉着，冷静！: 噗，这你都信！
  * @LastEditors: HANGNAG
- * @LastEditTime: 2021-12-15 23:37:33
+ * @LastEditTime: 2022-03-08 15:01:56
  * @FilePath: \ACM_vscode\C++\test.cpp
  */
-#include<iostream>
-#include<fstream>
+#include<algorithm>
 #include<string.h>
+#include<iostream>
+#include<stdio.h>
+#include<string>
+#include<math.h>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<map>
+#include<set>
+#define emplace_back push_back
+#define pb push_back
 using namespace std;
-int index(char *s1, char *s2)
-{
-    int k = strlen(s1);
-    int m = strlen(s2);
-    for (int i = 0; i < m;i++)
-    {
-        int flog = 0;
-        for (int j = 0; j <k;j++)
-        {
-            if(i+j<m&&s1[j]!=s2[i+j])
-            {
-                flog = 1;
-                break;
-            }
-        }
-        if(!flog)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
+typedef long long LL;
+const int mod = 1e9 + 7;
+const double eps = 1e-6;
+const int inf = 0x3f3f3f3f;
+const int N = 2e5 + 10;
+
 int main()
 {
-    //cout << index(s1, s2) << endl;
-    ofstream ou;
-    ou.open("a.txt",ios::out);
-    char s[1024];
-    char ss[] ="";
-    while(1)
+    int num = 0;
+    int *p;
+    while(p=new int[1000000])
     {
-        cin.getline(s,1000,'\n');
-        cout << s<<"++++++++" << endl;
-        if(strcmp(ss,s)==0)
-        {
-            break;
-        }
-        
-        ou << s << '\n';
+        num += 1000000;
+        cout << num/1024/1024 << endl;
     }
-    ou.close();
-    //in.open("a.txt");
-
     return 0;
 }
---
+++ C++/数据结构/表达式求值.cpp
@@ -0,0 +1,162 @@
+/*
+ * @LastEditors: HANGNAG
+ * @LastEditTime: 2022-03-21 09:24:14
+ * @FilePath: \ACM_vscode\C++\数据结构\表达式求值.cpp
+ */
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <set>
+#include <stack>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <vector>
+#define emplace_back push_back
+#define pb push_back
+using namespace std;
+typedef long long LL;
+const int mod = 1e9 + 7;
+const double eps = 1e-6;
+const int inf = 0x3f3f3f3f;
+const int N = 2e5 + 10;
+string s;
+int zhua(char s1)
+{
+    if (s1 == '+' || s1 == '-')
+    {
+        return 1;
+    }
+    else if (s1 == '*' || s1 == '/')
+    {
+        return 2;
+    }
+    else if (s1 == '(')
+    {
+        return 0;
+    }
+}
+int yunsuan(int a,int b,char ss)
+{
+    if(ss=='+')
+    {
+        return (a + b);
+    }
+    else if(ss=='-')
+    {
+        return (a - b);
+    }
+    else if(ss=='*')
+    {
+        return (a * b);
+    }
+    else
+    {
+        return (a / b);
+    }
+}
+int main()
+{
+    cin >> s;
+    stack<char> q;
+    stack<char> hzui;
+    stack<char> hz;
+    stack<int> num;
+    int k = 0, flog = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            flog = 1;
+            hzui.push(s[i]);
+        }
+        else
+        {
+            if(flog)
+            {
+                hzui.push('|');
+                flog = 0;
+            }
+            if (s[i] == '(')
+            {
+                q.push(s[i]);
+            }
+            else if (s[i] == ')')
+            {
+                while (1)
+                {
+                    char k = q.top();
+                    q.pop();
+                    if (k == '(')
+                    {
+                        break;
+                    }
+                    hzui.push(k);
+                }
+            }
+            else
+            {
+                while (!q.empty())
+                {
+                    char k = q.top();
+                    if (zhua(s[i]) > zhua(k))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        q.pop();
+                        hzui.push(k);
+                    }
+                }
+                q.push(s[i]);
+            }
+        }
+    }
+    while (!q.empty())
+    {
+        hzui.push(q.top());
+        q.pop();
+    }
+    while (!hzui.empty())
+    {
+        hz.push(hzui.top());
+         cout << hzui.top();
+        hzui.pop();
+    }
+    cout << endl;
+    int ans = 0;
+    while(!hz.empty())
+    {
+        char ch = hz.top();
+        hz.pop();
+        if(ch>='0'&&ch<='9')
+        {
+            k = k * 10 + ch - '0';
+        }
+        else if(ch=='|')
+        {
+            num.push(k);
+            k = 0;
+        }
+        else
+        {
+            int a = num.top();
+            num.pop();
+            int b = num.top();
+            num.pop();
+            cout << a << " " << b << " " << ch << endl;
+            b = yunsuan(a, b, ch);
+            num.push(b);
+
+        }
+    }
+    cout << num.top() << endl;
+    return 0;
+}
+/*
+(1+82+37)*51+64*(2+3)
+(1+2+3)*5+6*(2+3)
+*/
